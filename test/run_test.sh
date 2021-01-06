cbe7ba7988dfcf1431f9681775c8492283402295
Omer Caspi <omer.caspi@gmail.com>
2021-01-06 15:21:04 +0200

    Project re-structure
    Make staff a bi more tidy. Move all the test code, which most of this
    project, into a separate folder. Slightly change the test code so it
    will copy the module.inc.mk file into the relevant test folders when
    testing the build system.
    
    Signed-off-by: Omer Caspi <omer.caspi@gmail.com>

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 SCRIPT_PATH=$(readlink -f ${BASH_SOURCE[0]})
-SCRIPT_DIR=$(dirname ${SCRIPT_PATH})
-PROJECT_DIR=${SCRIPT_DIR}/..
+SBS_TEST_DIR=$(dirname ${SCRIPT_PATH})
+SBS_DIR=${SBS_TEST_DIR}/..
+SBS_BINARIES_TEST_DIR=${SBS_TEST_DIR}/binaries
+SBS_ORDER_TEST_DIR=${SBS_TEST_DIR}/order
 
 outfile=$(mktemp)
-cd ${PROJECT_DIR}
+cp ${SBS_DIR}/module.inc.mk ${SBS_BINARIES_TEST_DIR}
+cd ${SBS_BINARIES_TEST_DIR}
 
 set -e
 echo "Cleaning binaries"
@@ -13,20 +16,21 @@ echo "Building binaries"
 make -j | grep -E "^Building" >> ${outfile}
 echo "Checking binaries output"
 
-export LD_LIBRARY_PATH=${PROJECT_DIR}/lib
+export LD_LIBRARY_PATH=${SBS_BINARIES_TEST_DIR}/lib
 
-${PROJECT_DIR}/prog/obj/dbg/prog >> ${outfile}
-${PROJECT_DIR}/dynlib/dynlib_tester/obj/dbg/dyn_tester >> ${outfile}
+${SBS_BINARIES_TEST_DIR}/prog/obj/dbg/prog >> ${outfile}
+${SBS_BINARIES_TEST_DIR}/dynlib/dynlib_tester/obj/dbg/dyn_tester >> ${outfile}
 
-export LD_LIBRARY_PATH=${PROJECT_DIR}/prog2/prog2_dynlib/obj/dbg
-${PROJECT_DIR}/prog2/obj/dbg/prog2 >> ${outfile}
+export LD_LIBRARY_PATH=${SBS_BINARIES_TEST_DIR}/prog2/prog2_dynlib/obj/dbg
+${SBS_BINARIES_TEST_DIR}/prog2/obj/dbg/prog2 >> ${outfile}
 
-diff -u ${SCRIPT_DIR}/expected_output.txt ${outfile}
+diff -u ${SBS_TEST_DIR}/expected_binaries_output.txt ${outfile}
 
-cd ${PROJECT_DIR}/order
+cp ${SBS_DIR}/module.inc.mk ${SBS_ORDER_TEST_DIR}
+cd ${SBS_ORDER_TEST_DIR}
 echo "Cleaning build-order"
 make -j clean | sed -e 's/00./00X/g' &> ${outfile}
 echo "Building build-order"
 make -j | sed -e 's/00./00X/g' &>> ${outfile}
 echo "Testing build-order output"
-diff -u ${SCRIPT_DIR}/expected_order_output.txt ${outfile}
+diff -u ${SBS_TEST_DIR}/expected_order_output.txt ${outfile}
