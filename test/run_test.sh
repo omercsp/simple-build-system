8ed87a1d020d1b01b3ff5aabf70afe53769e3ad2
Omer Caspi <omer.caspi@gmail.com>
2021-04-24 20:14:48 +0300

    All tests refer to main module.inc.mk
    Instead of linking the module.inc.mk into the test directory, just use
    the correct path to the project root directory.
    
    Signed-off-by: Omer Caspi <omer.caspi@gmail.com>

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,29 +1,6 @@
 #!/bin/bash
 SCRIPT_PATH=$(readlink -f ${BASH_SOURCE[0]})
 SBS_TEST_DIR=$(dirname ${SCRIPT_PATH})
-SBS_DIR=${SBS_TEST_DIR}/..
-SBS_MODULE_INC_MK_FNAME=module.inc.mk
-SBS_MODULE_INC_FILE=${SBS_DIR}/${SBS_MODULE_INC_MK_FNAME}
-
-_setup_module_inc_mk()
-{
-	local dir=${1}
-	local file=${dir}/${SBS_MODULE_INC_MK_FNAME}
-
-	if [[ -z ${dir} ]]; then
-		echo "Missing direcotry argument"
-		return 1
-	fi
-
-	if [[ ! -d ${dir} ]]; then
-		echo "Directory argument '${dir}' isn't a directory"
-		return 1
-	fi
-	[[ ${file} -ef ${SBS_MODULE_INC_FILE} ]] && return
-	rm -f ${file}
-	# Using hard links so when developing, there's no confusion
-	ln ${SBS_MODULE_INC_FILE} ${file}
-}
 
 outfile=$(mktemp)
 if [[ -n $* ]]; then
@@ -34,7 +11,6 @@ fi
 for d in ${test_dirs[@]}; do
 	test_dir=${SBS_TEST_DIR}/${d}
 	echo "Running test at '${d}'"
-	_setup_module_inc_mk ${test_dir}
 	if [[ ${SBS_TEST_VERBOSE} -eq 1 ]]; then
 		${test_dir}/run_test.sh | tee ${outfile}
 		ret=$?
