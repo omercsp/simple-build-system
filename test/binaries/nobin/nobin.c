c9d8c921d96c1f2f6aeaca7ae7c262baa76f7023
Omer Caspi <omer.caspi@gmail.com>
2021-08-06 13:43:58 +0300

    test: nobin test streamline
    Signed-off-by: Omer Caspi <omer.caspi@gmail.com>

--- test/binaries/nobin/nobin.c
+++ test/binaries/nobin/nobin.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int none_object_func(void)
+int nobin_func(void)
 {
 	printf("function=%s\n", __func__);
 	return 0;
--- test/binaries/nobin/object.h
+++
@@ -1,7 +0,0 @@
-#ifndef __STATIC_H__
-#define __STATIC_H__
-
-int none_object_func(void);
-
-#endif
-
