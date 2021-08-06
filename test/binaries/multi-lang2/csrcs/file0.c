e6fe371596e54da37d02fe9889717907528b40ea
Omer Caspi <omer.caspi@gmail.com>
2021-08-06 13:25:49 +0300

    test: multi-lang2 test
    A slightly more complicated test with both C and C++ source files. The
    linker (MODULE_LD), is chosen automatically by SBS. Note that none-C++
    linker (i.e. gcc instead of g++) will create a linking error, as the it
    (the linker) will not find libstdc++ library symbols to link with.
    
    Signed-off-by: Omer Caspi <omer.caspi@gmail.com>

---
+++ test/binaries/multi-lang2/csrcs/file0.c
@@ -0,0 +1,13 @@
+#include <header.h>
+#include <sbstest-base.h>
+#include <stdlib.h>
+
+void cfile_func0(void)
+{
+	call_func(cppfile_func0);
+}
+
+void cfile_func1(void)
+{
+	call_func(cppfile_func1);
+}
---
+++ test/binaries/multi-lang2/run_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+here=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+prog=$(basename ${here})
+
+cd ${here}
+make -j clean
+make -j
+obj/dbg/${prog}
+
