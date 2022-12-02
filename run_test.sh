be905dbabb80aca25419bc02a5c01d79277a4335
Omer Caspi <omer.caspi@gmail.com>
2022-12-02 14:26:33 +0200

    sbs: some test infra love
    Some modifications to the test infrastructure, mostly around not having
    to manually add a test case each time. Also slightly simplified the
    test directories handling.
    
    Signed-off-by: Omer Caspi <omer.caspi@gmail.com>

--- run_test.sh
+++ run_test.sh
@@ -5,20 +5,19 @@ SBS_TEST_DIR=$(dirname ${SCRIPT_PATH})/test
 if [[ -n $* ]]; then
 	test_dirs=($*)
 else
-	test_dirs=("tutorial" "binaries" "order")
+	test_dirs=$(find ${SBS_TEST_DIR} -maxdepth 1 -type d -not -path ${SBS_TEST_DIR})
 fi
 
 for flav in dbg rel; do
 	outfile=$(mktemp)
-	for d in ${test_dirs[@]}; do
-		test_dir=${SBS_TEST_DIR}/${d}
+	for dir in ${test_dirs}; do
 		export MODULE_FLAV=${flav}
-		echo "Running test at '${d}' (${flav})"
+		echo "Running test at '$(basename ${dir})' (${flav})"
 		if [[ ${SBS_TEST_VERBOSE} -eq 1 ]]; then
-			${test_dir}/run_test.sh | tee ${outfile}
+			${dir}/run_test.sh | tee ${outfile}
 			ret=$?
 		else
-			${test_dir}/run_test.sh &> ${outfile}
+			${dir}/run_test.sh &> ${outfile}
 			ret=$?
 		fi
 		if [[ ${ret} -ne 0 ]]; then
@@ -29,7 +28,7 @@ for flav in dbg rel; do
 			exit 1
 		fi
 
-		diff -u ${test_dir}/expected_output.txt ${outfile} || exit 1
+		diff -u ${dir}/expected_output.txt ${outfile} || exit 1
 		[[ ${SBS_TEST_VERBOSE} -eq 1 ]] && echo
 	done
 
