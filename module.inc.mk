760b8177230b7e8a3df51961c413b8f38cac7146
Omer Caspi <omer.caspi@gmail.com>
2023-03-23 21:18:20 +0200

    sbs: add small header to the makefile
    Signed-off-by: Omer Caspi <omer.caspi@gmail.com>

--- module.inc.mk
+++ module.inc.mk
@@ -1,3 +1,6 @@
+# Simple Build Systen, written by Omer Caspi
+# https://github.com/omercsp/simple-build-system
+
 SBS_PROJ_ROOT := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
 SHELL := /usr/bin/bash
 ECHO := /usr/bin/echo
