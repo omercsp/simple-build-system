1fda6a40a58a91a710bb57a354c3759c517f3735
Omer Caspi <omer.caspi@gmail.com>
2023-03-31 14:36:24 +0300

    test: test all binaries with gcc and clang
    Repeat all the binary tests with both gcc and clang instead of having an
    arbitrary test run in with clang.
    
    Signed-off-by: Omer Caspi <omer.caspi@gmail.com>

--- test/002_binaries/run_test.sh
+++ test/002_binaries/run_test.sh
@@ -7,37 +7,46 @@ if [[ -z ${TEST_FLAV} ]]; then
 	[[ ${FULL_TEST} -eq 1 ]] && TEST_FLAV+=" rel"
 fi
 
+TEST_CSUITE=${TEST_CSUITE:-}
+if [[ -z ${TEST_CSUITE} ]]; then
+	TEST_CSUITE="gcc"
+	[[ ${FULL_TEST} -eq 1 ]] && TEST_CSUITE+=" clang"
+fi
+
 cd ${here}
 
 
-for flav in ${TEST_FLAV}; do
-	echo "Testing '${flav}'"
-	echo "================="
-	export MODULE_FLAV=${flav}
+for csuite in ${TEST_CSUITE}; do
+	for flav in ${TEST_FLAV}; do
+		echo "Testing '${csuite}/${flav}'"
+		echo "================="
+		export MODULE_FLAV=${flav}
+		export MODULE_CSUITE=${csuite}
 
-	set -e
-	echo "Cleaning binaries"
-	echo "-----------------"
-	make -j clean  | grep -E "^Cleaning"
-	echo
+		set -e
+		echo "Cleaning binaries"
+		echo "-----------------"
+		make -j clean  | grep -E "^Cleaning"
+		echo
 
-	echo "Building binaries"
-	echo "-----------------"
-	make -j | grep -E "^Building"
-	echo
+		echo "Building binaries"
+		echo "-----------------"
+		make -j | grep -E "^Building"
+		echo
 
-	echo "Checking binaries output"
-	echo "------------------------"
+		echo "Checking binaries output"
+		echo "------------------------"
 
-	export LD_LIBRARY_PATH=${here}/lib/${flav}
-	prog/obj/${flav}/prog
-	dynlib/dynlib_tester/obj/${flav}/dyn_tester
-	prog-nobin/obj/${flav}/prog_nobin
-	prog-nobin-extern/obj/${flav}/prog-nobin-extern
+		export LD_LIBRARY_PATH=${here}/lib/${flav}
+		prog/obj/${flav}/prog
+		dynlib/dynlib_tester/obj/${flav}/dyn_tester
+		prog-nobin/obj/${flav}/prog_nobin
+		prog-nobin-extern/obj/${flav}/prog-nobin-extern
 
-	export LD_LIBRARY_PATH=${here}/prog2/prog2_dynlib/obj/${flav}
-	prog2/obj/${flav}/prog2
-	multi-lang/obj/${flav}/multi-lang
-	multi-lang2/obj/${flav}/multi-lang2
-	prog-with-out-of-tree-src/obj/${flav}/prog-with-out-of-tree-src
+		export LD_LIBRARY_PATH=${here}/prog2/prog2_dynlib/obj/${flav}
+		prog2/obj/${flav}/prog2
+		multi-lang/obj/${flav}/multi-lang
+		multi-lang2/obj/${flav}/multi-lang2
+		prog-with-out-of-tree-src/obj/${flav}/prog-with-out-of-tree-src
+	done
 done
